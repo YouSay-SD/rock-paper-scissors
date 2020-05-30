469a6e4c9c1a5e88971ea17ec4d16368dbbb4321
YouSay-SD <yousay.sd@gmail.com>
2020-05-29 22:12:26 -0300

    Init Header Component

---
+++ src/header.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const HeaderStyled = styled.div`
+
+`
+
+function Header() {
+    return (
+        <HeaderStyled>
+
+            Header
+
+        </HeaderStyled>
+    )
+}
+
+export default Header
