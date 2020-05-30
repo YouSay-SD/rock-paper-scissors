8077e42ca0a9c94d14deaf7f04f2bb4276cf8ba0
YouSay-SD <yousay.sd@gmail.com>
2020-05-30 00:33:13 -0300

    Added Styles for Header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import './App.css';
+import Header from './header' 
+import styled from 'styled-components'
+import Wrapper from './wrapper'
+
+const AppStyled = styled.main`
+  background-image: radial-gradient(circle at top, #1F3757 0%, #131537 100%);
+  min-height: 100vh;
+  padding: 2em;
+`
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <AppStyled>
+
+      <Wrapper>
+
+        <Header />
+
+      </Wrapper>
+
+    </AppStyled>
   );
 }
 
--- src/header.js
+++ src/header.js
@@ -2,14 +2,17 @@ import React from 'react'
 import styled from 'styled-components'
 
 const HeaderStyled = styled.div`
-
+    color: white;
+    border: 1px solid rgba(255, 255, 255, .29);
+    padding: 2em;
+    border-radius: 5px
 `
 
 function Header() {
     return (
         <HeaderStyled>
 
-            Header
+            <h1>Rock Paper Scissors</h1>
 
         </HeaderStyled>
     )
